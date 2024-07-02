54824d910f4c09c8fa8da154b542e30e4b119275
kangsugyeong <ahydroponicfarm@naver.com>
2024-07-02 17:27:20 +0900

    과제
    과제

---
+++ 과제.py
@@ -0,0 +1,15 @@
+name1 = input("이름을 입력하세요: ")
+age1 = input("나이를 입력하세요: ")
+mail1 = input("메일주소를 입력하세요: ")
+number1 = input("연락처를 입력하세요: ")
+
+name2 = input("이름을 입력하세요: ")
+age2 = input("나이를 입력하세요: ")
+mail2 = input("메일주소를 입력하세요: ")
+number2 = input("연락처를 입력하세요: ")
+
+dic_1 = {name1: [age1, mail1, number1]}
+dic_2 = {name2: [age2, mail2, number2]}
+
+print(dic_1)
+print(dic_2)
---
+++ 과제2.py
@@ -0,0 +1,3 @@
+exchange = {'달러': 1320, '엔화': 950, '위안': 182}
+chul = [13, 200, 13]
+print("철수가 가지고 있는 돈의 원화 가치는", exchange['달러'] * chul[0] + exchange['엔화'] * chul[1] + exchange['위안'] * chul[2], "원 입니다.")
